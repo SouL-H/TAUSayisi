2249a135d88d3f317cd56566b8778cdb6f4ff62c
Mehmet AYDIN <mhmtaydinw@gmail.com>
2021-01-17 01:50:37 +0300

    TAU Sayisi

---
+++ TAUSayısı/TAUSayısı/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAUSayısı
+{
+    class Program
+    {
+        //Main 
+        static void Main(string[] args)
+        {
+            int sayi;
+            while (true)
+            {
+                Console.Write("Hangi sayıya kadar olan TAU Sayıları listelensin = ");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n");
+                if (sayi < 0) {
+                    Console.WriteLine("Lütfen 0 dan küçük sayı girmeyiniz");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            TAUSayisi tau = new TAUSayisi();
+            tau.TAUSayi(sayi);
+        }
+
+
+    }
+}
---
+++ TAUSayısı/TAUSayısı/TAUSayisi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAUSayısı
+{
+    class TAUSayisi
+    {
+        public void TAUSayi(int sayi)
+        {
+            Console.WriteLine("***********************************************************************************************************************");
+            Console.WriteLine("*******************************                    1 - " + sayi + " arasındaki                 *********************************");
+            Console.WriteLine("***********************************************************************************************************************");
+            Console.WriteLine("************************* @@@@@@@  @@@@@@  @@@  @@@       @@@@@@  @@@@@@  @@@ @@@ @@@  @@@@@@ @@@ *********************");
+            Console.WriteLine("*************************   @@!   @@!  @@@ @@!  @@@      !@@     @@!  @@@ @@! !@@ @@! !@@     @@! *********************");
+            Console.WriteLine("*************************   @!!   @!@!@!@! @!@  !@!       !@@!!  @!@!@!@!  !@!@!  !!@  !@@!!  !!@ *********************");
+            Console.WriteLine("*************************   !!:   !!:  !!! !!:  !!!          !:! !!:  !!!   !!:   !!:     !:! !!: *********************");
+            Console.WriteLine("*************************    :     :   : :  :.:: :       ::.: :   :   : :   .:    :   ::.: :  :   *********************");
+            Console.WriteLine("***********************************************************************************************************************");
+            Console.WriteLine("\n");
+            int counter = 0;
+            int divisorCounter;
+
+            for (int i = 1; i < sayi; i++)
+            {
+                divisorCounter = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        divisorCounter++;
+                    }
+                }
+                if (i % divisorCounter == 0)
+                {
+                    Console.Write(i + "    ");
+                    counter++;
+                }
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("\nToplamda " + counter + " tane Tau sayısı vardır.");
+            Console.Read();
+        }
+    }
+}
